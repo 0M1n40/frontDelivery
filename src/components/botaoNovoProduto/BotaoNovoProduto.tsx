919a52efb958dbedeae2a756a26797f51977f4f6
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-22 14:51:56 -0300

    feat(BotaoNovoProduto): implement button to add new product)

---
+++ src/components/botaoNovoProduto/BotaoNovoProduto.tsx
@@ -0,0 +1,11 @@
+import './BotaoNovoProduto.css';
+
+function BotaoNovoProduto() {
+  return (
+    <button className="botao-novo-produto">
+      <span className="button-text">NOVO PRODUTO</span>
+    </button>
+  );
+}
+
+export default BotaoNovoProduto;
