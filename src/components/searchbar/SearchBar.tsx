e67c2004942fa8c70bac0d3790d456e9290227a7
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-22 11:50:43 -0300

    feat(SearchBar): create basic search bar

---
+++ src/components/searchbar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import './SearchBar.css';
+
+function SearchBar() {
+    return (
+        <div className="searchbar">
+          <input type="text" className="search-input" />
+        </div>
+      );
+}
+
+export default SearchBar
