7eeebfcc51919333189e636de8210a11417eed26
LA1SA0 <ferreirlaisa@gmail.com>
2025-04-23 12:25:33 -0300

    Revert "Estilização FormCategoria"
    This reverts commit d84b9826d9ab0761950d7853b63a86782d110e0b.

--- src/components/categorias/cardcategoria/CardCategoria.tsx
+++ src/components/categorias/cardcategoria/CardCategoria.tsx
@@ -7,13 +7,32 @@ interface CardCategoriaProps {
 
 function CardCategoria({ categoria }: CardCategoriaProps) {
     return (
-        <Link 
-            to={`/categoria/${categoria.id}`}
-            className="bg-red-600 text-white font-medium text-center py-3
-                       rounded-md hover:bg-red-700 transition duration-300 w-full"
-        >
-            {categoria.nome}
-        </Link>
+        <div className="flex flex-col rounded-md overflow-hidden">
+            <Link 
+                to={`/listacategorias/${categoria.id}`} 
+                className="bg-red-600 text-white font-medium text-center py-3 px-5 
+                hover:bg-red-700 transition duration-300 w-full"
+            >
+                Categorias
+            </Link>
+
+            <div className="flex text-sm">
+                <Link 
+                    to={`/cadastrarcategoria/${categoria.id}`} 
+                    className="w-1/2 text-slate-100 bg-indigo-500 hover:bg-indigo-700 
+                        flex items-center justify-center py-1.5 transition duration-300"
+                >
+                    Editar
+                </Link>
+                <Link 
+                    to={`/deletarcategoria/${categoria.id}`} 
+                    className="text-slate-100 bg-red-500 hover:bg-red-700 w-1/2 
+                        flex items-center justify-center py-1.5 transition duration-300"
+                >
+                    Deletar
+                </Link>
+            </div>
+        </div>
     );
 }
 
