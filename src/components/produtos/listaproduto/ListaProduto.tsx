f00219ab7c4f230c862f00dd5400542f6fb2465e
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-23 10:02:17 -0300

    fix(ListaProduto): fix route)

--- src/components/produtos/listaproduto/ListaProduto.tsx
+++ src/components/produtos/listaproduto/ListaProduto.tsx
@@ -1,50 +1,28 @@
 
 import { useState, useEffect } from "react";
-import { DNA } from "react-loader-spinner";
 import Produto from "../../../models/Produto";
 import CardProduto from "../cardproduto/CardProduto";
 import { buscar } from "../../../service/Service";
 
 function ListaProduto() {
 
-    // const navigate = useNavigate();
-
     const [produto, setProduto] = useState<Produto[]>([]);
-
-
 
     async function buscarProduto() {
         try {
-            await buscar('/produto', setProduto)
+            await buscar('/produtos', setProduto)
 
         } catch (error: any) {
             console.log(error)
         }
     }
 
-    // useEffect(() => {
-    //     if (token === '') {
-    //         alert('Você precisa estar logado')
-    //         navigate('/');
-    //     }
-    // }, [token])
-
     useEffect(() => {
         buscarProduto()
-    }, [produto.length])
+    }, [])
 
     return (
         <>
-            {produto.length === 0 && (
-                <DNA
-                    visible={true}
-                    height="200"
-                    width="200"
-                    ariaLabel="dna-loading"
-                    wrapperStyle={{}}
-                    wrapperClass="dna-wrapper mx-auto"
-                />
-            )}
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col mx-2">
                     <div className='container mx-auto my-4 
