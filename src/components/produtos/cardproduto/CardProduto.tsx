a658534a9322f1bd38bb471d18b158cff78fdd78
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-23 10:18:18 -0300

    feat(CardProduto): create new style

--- src/components/produtos/cardproduto/CardProduto.tsx
+++ src/components/produtos/cardproduto/CardProduto.tsx
@@ -1,48 +1,30 @@
 import { Link } from 'react-router-dom'
 import Produto from '../../../models/Produto'
+import './CardProduto.css'
 
 interface CardProdutoProps {
-    produto: Produto
+  produto: Produto
 }
 
 function CardProduto({ produto }: CardProdutoProps) {
-    return (
-        <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
-                
-            <div>
-                <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
-                    <img
-                        src={produto.usuario?.foto}
-                        className='h-12 rounded-full'
-                        alt={produto.usuario?.nome} />
-                    <h3 className='text-lg font-bold text-center uppercase'>
-                        {produto.usuario?.nome}
-                    </h3>
-                </div>
-                <div className='p-4 '>
-                    <h4 className='text-lg font-semibold uppercase'>{produto.nome}</h4>
-                    <p>{produto.preco}</p>
-                    <p>Categoria: {produto.categoria.categoria}</p>
-                    <p>Data: {new Intl.DateTimeFormat(undefined, {
-                        dateStyle: 'full',
-                        timeStyle: 'medium',
-                    }).format(new Date(produto.estoque))}</p>
-                </div>
-            </div>
-            <div className="flex">
-                <Link to='' 
-                    className='w-full text-white bg-indigo-400 
-                    hover:bg-indigo-800 flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
-                <Link to='' 
-                    className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card-produto">
+      <img
+        alt={produto.nome}
+        className="imagem-produto"
+      />
+
+      <div className="info-container">
+        <h2 className="nome-produto">{produto.nome}</h2>
+        <span className="preco-produto">R$ {produto.preco}</span>
+      </div>
+
+      <div className="botoes-container">
+        <Link to={`/editarproduto/${produto.id}`} className="botao editar">Editar</Link>
+        <Link to={`/deletarproduto/${produto.id}`} className="botao deletar">Deletar</Link>
+      </div>
+    </div>
+  )
 }
 
 export default CardProduto
