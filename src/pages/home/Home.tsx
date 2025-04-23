f97fea126a23cae51fc836791f21c999c8cf1362
Mina Yura <minammonteiro2@gmail.com>
2025-04-23 00:56:25 -0300

    att home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import home from "../../utils/img/Home.png";
 import BotaoNovoProduto from "../../components/botaoNovoProduto/BotaoNovoProduto";
 
 
+
 function Home() {
   return (
     <>
@@ -13,14 +14,13 @@ function Home() {
             backgroundImage: `url(${home})`,
           }}
         >
-      
-
           <BotaoNovoProduto />
         </div>
 
         <div className=" flex flex-col items-center justify-center gap-4 mt-10 ">
           <h2 className="text-center text-3xl font-semibold text-gray-800">
             Meus produtos
+            
           </h2>
         </div>
       </div>
