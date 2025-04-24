b288051c94a784918d530514cb0b10661b8ad53e
carolgonc <carolinypeixoto@outlook.com>
2025-04-24 17:26:59 -0300

    Finalização do projeto, deletar produtos e categorias, cadastrar e atualizar categorias

--- src/components/categorias/cadastrarcategoria/CadastrarCategoria.tsx
+++ src/components/categorias/cadastrarcategoria/CadastrarCategoria.tsx
@@ -1,39 +1,39 @@
-function CadastrarCategoria() {
-    return (
-      <div className="container w-1/3 mx-auto">
-        <h1 className="text-4xl text-center my-4">Cadastrar Categoria</h1>
-        <p className="text-center font-semibold mb-4">
-          Preencha as informações abaixo para cadastrar uma nova categoria.
-        </p>
-        <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
-          <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
-            Nova Categoria
-          </header>
-          <form className="p-8 bg-slate-200 space-y-4">
-            <div>
-              <label className="block text-lg font-medium mb-2" htmlFor="descricao">
-                Descrição da Categoria
-              </label>
-              <input
-                id="descricao"
-                type="text"
-                className="w-full p-2 border rounded-md"
-                placeholder="Digite a descrição da categoria"
-              />
-            </div>
-            <div className="flex">
-              <button
-                type="submit"
-                className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 py-2"
-              >
-                Cadastrar
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+// function CadastrarCategoria() {
+//     return (
+//       <div className="container w-1/3 mx-auto">
+//         <h1 className="text-4xl text-center my-4">Cadastrar Categoria</h1>
+//         <p className="text-center font-semibold mb-4">
+//           Preencha as informações abaixo para cadastrar uma nova categoria.
+//         </p>
+//         <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+//           <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
+//             Nova Categoria
+//           </header>
+//           <form className="p-8 bg-slate-200 space-y-4">
+//             <div>
+//               <label className="block text-lg font-medium mb-2" htmlFor="descricao">
+//                 Descrição da Categoria
+//               </label>
+//               <input
+//                 id="descricao"
+//                 type="text"
+//                 className="w-full p-2 border rounded-md"
+//                 placeholder="Digite a descrição da categoria"
+//               />
+//             </div>
+//             <div className="flex">
+//               <button
+//                 type="submit"
+//                 className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 py-2"
+//               >
+//                 Cadastrar
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   }
   
-  export default CadastrarCategoria;
+//   export default CadastrarCategoria;
   
--- src/components/categorias/formcategoria/FormCategoria.tsx
+++ src/components/categorias/formcategoria/FormCategoria.tsx
@@ -1,28 +1,38 @@
-import { ChangeEvent, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Categoria from "../../../models/Categoria";
 import { atualizar, buscar, cadastrar } from "../../../service/Service";
+import { RotatingLines } from "react-loader-spinner";
+import { ChangeEvent, useEffect, useState } from "react";
+import "./formcategoria.css";
+import BotaoCadastrar from "../../botaoCadastrar/BotaoCadastrar";
+import BotaoAtualizar from "../../botaoCadastrar/BotaoAtualizar";
 
+// interface FormCategoriaProps {
+//   setAtualizarLista: React.Dispatch<React.SetStateAction<boolean>>;
+// }
+
+//{ setAtualizarLista }: FormCategoriaProps
 function FormCategoria() {
   const navigate = useNavigate();
-  const { id } = useParams<{ id: string }>();
-
-  const [categoria, setCategoria] = useState<Categoria>({ nome: "" });
+  const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { id } = useParams<{ id: string }>();
 
   async function buscarPorId(id: string) {
     try {
-      await buscar(`/categorias/${id}`, setCategoria);
-    } catch (error) {
-      alert("Erro ao buscar categoria");
-    }
+      await buscar(`/categorias/${id}`, setCategoria)
+  } catch (error: any) {
+      if (error.toString().includes('403')) {
+      }
   }
+}
 
-  useEffect(() => {
-    if (id !== undefined) {
-      buscarPorId(id);
-    }
-  }, [id]);
+
+useEffect(() => {
+  if (id !== undefined) {
+      buscarPorId(id)
+  }
+}, [id])
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setCategoria({
@@ -32,54 +42,71 @@ function FormCategoria() {
   }
 
   function retornar() {
-    navigate("/categorias");
-  }
+    navigate("/categorias")
+}
 
-  async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setIsLoading(true);
+async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
+  e.preventDefault()
+  setIsLoading(true)
 
-    try {
-      if (id !== undefined) {
-        await atualizar(`/categorias`, categoria, setCategoria);
-        alert("Categoria atualizada com sucesso!");
-      } else {
-        await cadastrar(`/categorias`, categoria, setCategoria);
-        alert("Categoria cadastrada com sucesso!");
+  if (id !== undefined) {
+      try {
+          await atualizar(`/categorias`, categoria, setCategoria)
+          alert('A Categoria foi atualizada com sucesso!')
+      } catch (error: any) {
+          if (error.toString().includes('403')) {
+          } else {
+              alert('Erro ao atualizar o tema.')
+          }
+
       }
-    } catch (error) {
-      alert("Erro ao salvar a categoria.");
-    } finally {
-      setIsLoading(false);
-      retornar();
-    }
+  } else {
+      try {
+          await cadastrar(`/categorias`, categoria, setCategoria)
+          alert('A categoria foi cadastrado com sucesso!')
+      } catch (error: any) {
+          if (error.toString().includes('403')) {
+          } else {
+              alert('Erro ao cadastrar a categoria.')
+          }
+
+      }
   }
+  retornar()
+}
 
   return (
-    <div className="container flex flex-col items-center justify-center mx-auto mt-40">
-      <h1 className="text-4xl text-center my-8">
-        {id === undefined ? "Cadastrar Categoria" : "Editar Categoria"}
+    <div className="form-categoria-container">
+      <h1 className="form-titulo">
+        {id === undefined ? "Cadastrar nova Categoria" : "Editar Categoria"}
       </h1>
 
-      <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
-        <div className="flex flex-col gap-2">
+      <form className="form-categoria" onSubmit={gerarNovaCategoria}>
+        <div className="form-campo">
           <label htmlFor="nome">Nome da Categoria</label>
           <input
             type="text"
-            placeholder="Digite o nome da categoria"
             name="nome"
-            className="border-2 border-slate-700 rounded p-2"
+            placeholder="Digite o nome da categoria"
             value={categoria.nome}
-            onChange={atualizarEstado}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
+          <hr />
         </div>
 
-        <button
-          className="rounded text-white bg-indigo-500 hover:bg-indigo-700 w-full py-2 flex justify-center"
-          type="submit"
-        >
-          {isLoading ? "Carregando..." : id === undefined ? "Cadastrar" : "Atualizar"}
-        </button>
+        {isLoading ? (
+          <div className="botao-form">
+            <RotatingLines
+              strokeColor="white"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="24"
+              visible={true}
+            />
+          </div>
+        ) : (
+          id === undefined ? <BotaoCadastrar /> : <BotaoAtualizar />
+        )}
       </form>
     </div>
   );
